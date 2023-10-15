0d08e3cab8fb215a63fac1d40f59939dabfc0adb
gantaAishwarya <aishwarya.ganta59@gmail.com>
2023-10-15 20:07:10 +0200

    nginx in docker using nginx-conf , application.ini

---
+++ wsgi.py
@@ -0,0 +1,7 @@
+import src.app as root
+from resources.config import server_config
+app = root.app
+
+if __name__ == '__main__':
+    #Running Flask application
+    app.run(host=server_config.host,port=server_config.port)
