11f1a627d8a2658bd02e5de7ce4d12103c14d36c
gantaAishwarya <aishwarya.ganta59@gmail.com>
2023-10-15 11:44:53 +0200

    adding a db model Computers

---
+++ data/dom.py
@@ -0,0 +1,10 @@
+from ..src.extensions import db
+
+class Computers(db.Model):
+    
+    #Defining database columns based on assumptions
+    MAC = db.Column(db.String(17), primary_key=True) #General MAC address is 17 characters long including special characters
+    name = db.Column(db.String(80), unique=True, nullable=False) #General system name does not have any specific length
+    IPAddr = db.Column(db.String(50), nullable=False) #IP Addresscan have maximum 49 characters length
+    empAbr = db.Column(db.String(3)) #Given employee abbreviation is 3 characters long
+    description = db.Column(db.String(100)) #Description length is not specified so giving 100 characters length
